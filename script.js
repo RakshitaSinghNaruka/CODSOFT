515b3be20904a98cae3f276aa38b45a441ef06a6
RakshitaSinghNaruka <rakshitasingh222@gmail.com>
2024-07-15 20:07:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// java script file
+let display = document.getElementById('display');
+
+function appendCharacter(character) {
+    display.innerText += character;
+}
+
+function clearDisplay() {
+    display.innerText = '';
+}
+
+function deleteLast() {
+    display.innerText = display.innerText.slice(0, -1);
+}
+
+function calculateResult() {
+    try {
+        display.innerText = eval(display.innerText);
+    } catch (error) {
+        display.innerText = 'Error';
+    }
+}
